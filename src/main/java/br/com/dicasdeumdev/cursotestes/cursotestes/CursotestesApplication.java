55bcb05d9ae3159de7db8b2fef116a32cb0b922d
vanessa <estanislaunessa@gmail.com>
2023-04-13 15:45:06 -0300

    projeto iniciado

---
+++ src/main/java/br/com/dicasdeumdev/cursotestes/cursotestes/CursotestesApplication.java
@@ -0,0 +1,13 @@
+package br.com.dicasdeumdev.cursotestes.cursotestes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursotestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursotestesApplication.class, args);
+	}
+
+}
