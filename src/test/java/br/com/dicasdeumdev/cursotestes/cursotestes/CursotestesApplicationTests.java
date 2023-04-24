7e42439f29e312bb329edbc3851e4d7d90d32232
vanessa <estanislaunessa@gmail.com>
2023-04-24 16:21:56 -0300

    teste para classe principal

--- src/test/java/br/com/dicasdeumdev/cursotestes/cursotestes/CursotestesApplicationTests.java
+++ src/test/java/br/com/dicasdeumdev/cursotestes/cursotestes/CursotestesApplicationTests.java
@@ -3,11 +3,14 @@ package br.com.dicasdeumdev.cursotestes.cursotestes;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import br.com.dicasdeumdev.cursotestes.CursotestesApplication;
+
 @SpringBootTest
 class CursotestesApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		CursotestesApplication.main(new String[] {});
 	}
 
 }
